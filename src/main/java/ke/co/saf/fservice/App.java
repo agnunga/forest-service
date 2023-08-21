d7d8a6dcddeb569c8227bc38279b988c671c794d
Omondi Agunga <agunga3d@gmail.com>
2023-08-21 19:12:26 +0300

    adding spark and handlebar dependancies

--- src/main/java/ke/co/saf/fservice/App.java
+++ src/main/java/ke/co/saf/fservice/App.java
@@ -2,7 +2,7 @@ package ke.co.saf.fservice;
 
 // Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class App {
     public static void main(String[] args) {
         // Press ⌥⏎ with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
